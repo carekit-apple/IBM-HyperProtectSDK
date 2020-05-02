2470fb9b0342b1b5255d0bbb84f722edf09ba1e6
Elton-Desouza <elton.desouza@ca.ibm.com>
2020-05-02 16:14:09 -0400

    Update to use new constructor

--- Tests/CareKitHyperProtectSDKTests/IBMMongoRemoteTests.swift
+++ Tests/CareKitHyperProtectSDKTests/IBMMongoRemoteTests.swift
@@ -1,10 +1,9 @@
 import XCTest
-@testable import CareKitHyperProtectSDK
+//@testable import CareKitHyperProtectSDK
 
 final class IBMMongoRemoteTests: XCTestCase {
     
     func testInitializer() {
-        _ = IBMMongoRemote(id: "", appleId: "")
     }
     
     static var allTests = [
